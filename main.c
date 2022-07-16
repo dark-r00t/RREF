02e11c74b1eed0eaf7ee176a0859e01a89e8fb54
Jan D <79925808+jdarge@users.noreply.github.com>
2022-07-16 18:48:54 -0400

    Update main.c

--- main.c
+++ main.c
@@ -8,39 +8,32 @@ void printMatrix(double **matrix);
 void rref(double **matrix, int n, int m);
 void initMatrix(double **matrix, int n, int m);
 void rowSwap(double **matrix, int a, int b, int n, int m);
-void onesPivot(double** matrix, int row, int col, int m);
-void multAdd(double** matrix, int row1, int row2, int col, int m) ;
+void onesPivot(double** matrix, int row, int col, int m, double pivot);
+void multAdd(double** matrix, int row1, int row2, int col, int m, double remove) ;
 
 int main(void) {
 
 	double **matrix = (double**) malloc(sizeof(double*) * N);
 	for(int i = 0; i<N; i++) {
-
 		matrix[i] = (double*) malloc(sizeof(double) * M);
 	}
 
-	for(int i =0; i<16;i++) {										// 16	12	 8	 4
-																	// 12	9	 6 	 3 
-		int p = 16 - i - 1;											// 8    6    4   2   
-		matrix[i/4][i%4] = (p%4) + (p/4) + ((p%4) * (p/4)) + 1;		// 4    3    2   1
+	for(int i =0; i<16;i++) {
+		int p = 16 - i - 1;  
+		matrix[i/4][i%4] = (p%4) + (p/4) + ((p%4) * (p/4)) + 1;
 	}
 
 	printMatrix(matrix);
-
 	rref(matrix, N, M);
-
 	printMatrix(matrix);
 }	
 
 void printMatrix(double **matrix) {
 	
 	for(int i = 0; i<16; i++) {
-
 		printf("%.2lf\t", matrix[i/4][i%4]);
 		if((i+1)%4 == 0) printf("\n");
-	} 
-
-	printf("\n");
+	} printf("\n");
 }
 
 void rref(double **matrix, int n, int m) {
@@ -50,26 +43,17 @@ void rref(double **matrix, int n, int m) {
 	initMatrix(matrix, n, m);
 
 	for(row=0; row<n ;row++){
-
 		while( *(*(matrix+row)+col ) == 0 && col<m) col++;
 
-		onesPivot(matrix, row, col, m);
+		onesPivot(matrix, row, col, m, matrix[row][col]);
 
-		for(int j = row+1; j<n; j++){
+		for(int j = row+1; j<n; j++)
+	    	if( *(*(matrix + j)+col) )
+				multAdd(matrix, row, j, col, m, matrix[j][col]);
 
-	    	if( *(*(matrix + j)+col) ){
-
-				multAdd(matrix, row, j, col, m);
-	    	}
-		}
-
-		if(row){
-
-	    	for(int j = row;j>0;j--){
-
-				multAdd(matrix, row, j-1, col, m);
-	   		}
-		}
+		if(row)
+	    	for(int j = row;j>0;j--)
+				multAdd(matrix, row, j-1, col, m, matrix[j-1][col]);
 
 		col++;
     }
@@ -77,43 +61,33 @@ void rref(double **matrix, int n, int m) {
 
 void initMatrix(double **matrix, int n, int m) {
 
-	for(int i = 0; i < n-1; i++) {
-
-		for(int j = i+1; j < n; j++) {
-
-			if( *(*matrix+i) < *(*(matrix+j)) ) {
+	for(int i = 0; i < n-1; i++)
+		for(int j = i+1; j < n; j++)
+			if( *(*matrix+i) < *(*(matrix+j)) )
 				rowSwap(matrix, i, j, n, m);
-			}
-		}
-	}
 
 	printMatrix(matrix);
 }
 
 void rowSwap(double **matrix, int a, int b, int n, int m) {
+	
 	double* t = *(matrix+a);
 	*(matrix+a) = *(matrix+b);
     *(matrix+b) = t;
-
-    //*(matrix+a) += *(matrix+b) – (*(matrix+b) = *(matrix+a));
 }
 
-void onesPivot(double** matrix, int row, int col, int m) {
+void onesPivot(double** matrix, int row, int col, int m, double pivot) {
 
-	double pivot = *(*(matrix+row)+col); 
+	if(col>=m) return;
 
-    for(int i = col; i < m; i++){
-
-		*(*(matrix+row)+i) = (*(*(matrix+row)+i) / pivot);
-	}
+	matrix[row][col] = matrix[row][col] / pivot;
+	onesPivot(matrix, row, ++col, m, pivot);
 }
 
-void multAdd(double** matrix, int row1, int row2, int col, int m) {
+void multAdd(double** matrix, int row1, int row2, int col, int m, double remove) {
 
-	double remove = *(*(matrix+row2)+col);
+	if(col >= m) return;
 
-    for(int i = col; i < m; i++){
-
-		*(*(matrix+row2)+i) = (*(*(matrix+row1)+i)*remove*-1) + *(*(matrix+row2)+i);
-    }
+	matrix[row2][col] = (matrix[row1][col] * remove * -1) + matrix[row2][col];
+	multAdd(matrix, row1, row2, ++col, m, remove);
 }
