391393a430bb3c8e6726f6539d4c81d405bb56f7
Jan D <79925808+jdarge@users.noreply.github.com>
2022-07-16 20:16:44 -0400

    Update original_algo_for_square_only.c

--- original_algo_for_square_only.c
+++ original_algo_for_square_only.c
@@ -1,8 +1,20 @@
-for(int i =0; i<16;i++) {
-	int p = 16 - i - 1;  
-	matrix[i/4][i%4] = (p%4) + (p/4) + ((p%4) * (p/4)) + 1;
+#define SIZE 4
+
+/*
+GENERATES:
+16 12 8  4
+12 9  6  3
+8  6  4  2
+4  3  2  1
+*/
+for(int i =0; i<SIZE*SIZE;i++) {
+	int p = SIZE*SIZE - i - 1;  
+	matrix[i/SIZE][i%SIZE] = (p%SIZE) + (p/SIZE) + ((p%SIZE) * (p/SIZE)) + 1;
 }	
-	
+
+/*
+PRINTS A SIZE*SIZE MATRIX
+*/
 for(int i = 0; i<SIZE; i++) {
 	printf("%.2lf\t", matrix[i/SIZE][i%SIZE]);
 	if((i+1)%SIZE == 0) printf("\n");
