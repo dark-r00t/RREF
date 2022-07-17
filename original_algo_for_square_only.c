3fd510e480f0fa8d7ccb76958a5ed1832a4e1f11
Jan D <79925808+jdarge@users.noreply.github.com>
2022-07-16 20:20:10 -0400

    Update original_algo_for_square_only.c

--- original_algo_for_square_only.c
+++ original_algo_for_square_only.c
@@ -6,6 +6,13 @@ GENERATES:
 12 9  6  3
 8  6  4  2
 4  3  2  1
+
+REMOVING THE LINE: "int p = SIZE*SIZE - i - 1;"
+GENERATES:
+1  2  3  4
+2  4  6  8
+3  6  9 12
+4  8 12 16
 */
 for(int i =0; i<SIZE*SIZE;i++) {
 	int p = SIZE*SIZE - i - 1;  
